753f5294081565543ce2af599376911dc5c8c553
Steven Rick <srick@ucsd.edu>
2014-02-19 10:54:29 -0800

    added flags for histogram creation

--- syncMatch.py
+++ syncMatch.py
@@ -1,30 +1,37 @@
-'''
-Created on Feb 18, 2014
 
+'''
+Created on Jan 24, 2014
+ 
 @author: rick
 '''
-
-import shutil
+ 
 import csv
 import sys
 import os
 import re
+import shutil
+import string
 import fnmatch
 import numpy as np
 import pylab as P
-import matplotlib.pyplot as plt
 from collections import Counter
+import subprocess as sp
+from decimal import Decimal
+import Tkinter, tkFileDialog
 from subprocess import Popen, PIPE
 
-import Tkinter, tkFileDialog
 root = Tkinter.Tk()
 root.withdraw()
 
-
 encoding_cmd_strings = {
+'mp4': '~/../../opt/local/bin/ffmpeg -i {0} -y -acodec libfaac -vcodec mpeg4 {1}'
+}
+
+
+vamp_cmd_strings = {
 'prep': '~/../../opt/local/bin/ffmpeg -i {0} -ac 1 -ar 16000 {1}',
 'merge': '~/../../opt/local/bin/ffmpeg -i {0} -i {1} -filter_complex amerge {2}',
-'sync': './vamp-simple-host match-vamp-plugin.dylib:match {0} 3 -o {1}'
+'sync': 'vamp/./vamp-simple-host match-vamp-plugin.dylib:match {0} 3 -o {1}'
 }
 
 def ffmpeg_prep(source, target, encoding):
@@ -32,7 +39,7 @@ def ffmpeg_prep(source, target, encoding):
     to the desired encoding"""
     print 'Prep started'
     # Popen calls the ffmpeg process, and collects the standard out/error
-    p = Popen(encoding_cmd_strings[encoding].format(source, target),stdout=PIPE,stderr=PIPE,shell=True)
+    p = Popen(vamp_cmd_strings[encoding].format(source, target),stdout=PIPE,stderr=PIPE,shell=True)
     stdout, stderr = p.communicate(input=None)
     return stdout, stderr
 
@@ -42,7 +49,7 @@ def ffmpeg_merge(source1, source2, target, encoding):
     to the desired encoding"""
     print 'Merge started'
     # Popen calls the ffmpeg process, and collects the standard out/error
-    p = Popen(encoding_cmd_strings[encoding].format(source1, source2, target),stdout=PIPE,stderr=PIPE,shell=True)
+    p = Popen(vamp_cmd_strings[encoding].format(source1, source2, target),stdout=PIPE,stderr=PIPE,shell=True)
     stdout, stderr = p.communicate(input=None)
     return stdout, stderr
 
@@ -52,14 +59,20 @@ def vamp_sync(source, target, encoding):
     to the desired encoding"""
     print 'Vamp sync started'
     # Popen calls the ffmpeg process, and collects the standard out/error
-    p = Popen(encoding_cmd_strings[encoding].format(source, target),stdout=PIPE,stderr=PIPE,shell=True)
+    p = Popen(vamp_cmd_strings[encoding].format(source, target),stdout=PIPE,stderr=PIPE,shell=True)
     stdout, stderr = p.communicate(input=None)
     return stdout, stderr
 
 
 def findMode(data, flag):
     rawdata = np.genfromtxt(data,dtype='float',delimiter = ',',skiprows=0, skip_header=0, skip_footer=0, usecols=1,usemask=True, invalid_raise=False)
-
+    P.figure()
+    if flag == "KM":
+        P.title("Kinect/Morae")
+    if flag == "KS":
+        P.title("Kinect/SMI")
+    
+    P.hist(rawdata, 250, histtype='stepfilled')
     print "Finding Sync Point"
     
     nData = []
@@ -72,13 +85,9 @@ def findMode(data, flag):
         return "Morae,"+str((sync[0])[0])+"\n"
     if flag == "KS":
         return "SMI,"+str((sync[0])[0])+"\n"
-
-
-
-def syncPrep(directory):
-    kinectInput = directory+'/input/kinect.wav'
-    smiInput = directory+'/input/smi.avi'
-    moraeInput = directory+'/input/morae.wmv'
+    
+    
+def syncPrep(kinectInput, smiInput, moraeInput):
     tempOutput = directory+'/temp/'
     
     if not os.path.exists(tempOutput):
@@ -99,9 +108,7 @@ def syncPrep(directory):
     return tempOutput
 
 
-def txt2csv(directory):
-    tempdirectory = directory+'/temp'
-    print "clean sync txt"
+def syncTxt2csv(tempdirectory):
     for filename in (os.listdir(tempdirectory)):
         #print filename
         if fnmatch.fnmatch(filename, '*.txt'):
@@ -117,35 +124,74 @@ def txt2csv(directory):
             out_csv.writerows(in_txt)
 
 
-directory = tkFileDialog.askdirectory()
-tempdirectory = directory+'/temp'
-syncPrep(directory)
-header = "Data Stream, Sync Point (in seconds)\n"
-writeHeader = True
-
-txt2csv(directory)
-outputData = []
-outputData.append("Kinect,0\n")
-
-syncsavename = directory+"/syncData.csv"
-sync_file = r""+syncsavename+""
-output = open(sync_file,'wb')
-#print outputData
-
-for csvfile in os.listdir(tempdirectory):
-    #print csvfile
-    if fnmatch.fnmatch(csvfile, 'KM.csv'):
-        outputData.append(findMode(tempdirectory+'/'+csvfile, 'KM'))
-    if fnmatch.fnmatch(csvfile, 'KS.csv'):
-        outputData.append(findMode(tempdirectory+'/'+csvfile, 'KS'))
-
-if writeHeader:
-    output.write(header)
-    writeHeader = False
-for line in outputData:
-    #print line
-    output.write(line)
+def main():
+    directory = tkFileDialog.askdirectory()
+    root.destroy()
     
-#danger this last line deletes the temp folder
-shutil.rmtree(tempdirectory)
+    for dirContents in os.listdir(directory):
+        #print dirContents
+        if dirContents == 'Kinect':
+            kinectDirectory = directory+'/Kinect'
+            for filename in os.listdir(kinectDirectory):
+                if fnmatch.fnmatch(filename, '*.wav'):
+                    kinectAudioFile = os.path.join(kinectDirectory, filename)
+                    
+                    
+        if dirContents == 'Morae':
+            moraeDirectory = directory+'/Morae'
+            for filename in os.listdir(moraeDirectory):
+                if fnmatch.fnmatch(filename, '*.mp4'):
+                    moraeVideoFile = os.path.join(moraeDirectory, filename)
+                        
+        if dirContents == 'SMI':
+            smiDirectory = directory+'/SMI'
+            for filename in os.listdir(smiDirectory):
+                if fnmatch.fnmatch(filename, '*.mp4'):
+                    smiVideoFile = os.path.join(smiDirectory, filename)
+    
+    return directory, kinectAudioFile, smiVideoFile, moraeVideoFile
 
+
+def syncMatch(kinectAudio, smiVideo, moraeVideo):
+    tempSyncDir = syncPrep(kinectAudio, smiVideo, moraeVideo)
+    syncTxt2csv(tempSyncDir)
+    syncHeader = "Data Stream, Sync Point (in seconds)\n"
+    writeSyncHeader = True
+    syncOutputData = []
+    syncOutputData.append("Kinect,0\n")
+    
+    syncsavename = directory+"/syncData.csv"
+    sync_file = r""+syncsavename+""
+    syncOutput = open(sync_file,'wb')
+    
+    for csvfile in os.listdir(tempSyncDir):
+        #print csvfile
+        if fnmatch.fnmatch(csvfile, 'KM.csv'):
+            syncOutputData.append(findMode(tempSyncDir+'/'+csvfile, 'KM'))
+        if fnmatch.fnmatch(csvfile, 'KS.csv'):
+            syncOutputData.append(findMode(tempSyncDir+'/'+csvfile, 'KS'))
+    
+    if writeSyncHeader:
+        syncOutput.write(syncHeader)
+        writeSyncHeader = False
+    for line in syncOutputData:
+        #print line
+        syncOutput.write(line)
+    
+    syncOutput.close()
+    
+    #danger this last line deletes the temp folder
+    shutil.rmtree(tempSyncDir)
+    
+    print "Synchronization Complete"
+
+
+
+histoFlag = True
+
+
+directory, kinectAudioFile, smiVideoFile, moraeVideoFile = main()
+
+syncMatch(kinectAudioFile, smiVideoFile, moraeVideoFile)
+if histoFlag:
+    P.show()
