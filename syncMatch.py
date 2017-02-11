98b25dee1027349ba86841955352719c07682a0c
Steven Rick <srick@eng.ucsd.edu>
2017-02-10 21:14:52 -0800

    fixes some ugly OLD code

--- syncMatch.py
+++ syncMatch.py
@@ -10,7 +10,7 @@ import os
 import shutil
 import fnmatch
 import numpy as np
-import pylab as P
+import pylab
 from collections import Counter
 import Tkinter, tkFileDialog
 from subprocess import Popen, PIPE
@@ -22,7 +22,7 @@ root.withdraw()
 vamp_cmd_strings = {
 'prep': '~/../../opt/local/bin/ffmpeg -i {0} -ac 1 -ar 16000 {1}',
 'merge': '~/../../opt/local/bin/ffmpeg -i {0} -i {1} -filter_complex amerge {2}',
-'sync': '/Users/quick/Develop/QuickSessionProcessor/vamp/./vamp-simple-host match-vamp-plugin.dylib:match {0} 3 -o {1}' #hardcoding location of vamp files
+'sync': '/Users/danilogr/Projects/syncmatch/vamp/./vamp-simple-host match-vamp-plugin.dylib:match {0} 3 -o {1}' #hardcoding location of vamp files
 }
 
 
@@ -59,15 +59,11 @@ def vamp_sync(source, target, encoding):
     return stdout, stderr
 
 
-def findMode(data, flag):
-    rawdata = np.genfromtxt(data,dtype='float',delimiter = ',',skiprows=0, skip_header=0, skip_footer=0, usecols=1,usemask=True, invalid_raise=False)
-    P.figure()
-    if flag == "KM":
-        P.title("Kinect/Morae")
-    if flag == "KS":
-        P.title("Kinect/SMI")
-    
-    P.hist(rawdata, 250, histtype='stepfilled')
+def findMode(data):
+    rawdata = np.genfromtxt(data,dtype='float', delimiter = ',', skiprows=0, skip_header=0, skip_footer=0, usecols=1, usemask=True, invalid_raise=False)
+    pylab.figure()
+    pylab.hist(rawdata, 250, histtype='stepfilled')
+    pylab.savefig(os.path.join(os.path.dirname(os.path.dirname(data)), 'histo.png'))
     print "Finding Sync Point"
     
     nData = []
@@ -76,34 +72,22 @@ def findMode(data, flag):
         
     syncData = Counter(nData)
     sync = syncData.most_common(1)
-    if flag == "KM":
-        return "Morae,"+str((sync[0])[0])+"\n"
-    if flag == "KS":
-        return "SMI,"+str((sync[0])[0])+"\n"
+    return "Merge,"+str((sync[0])[0])+"\n"
     
     
-def syncPrep(directory, kinectInput, smiInput, moraeInput):
-    tempOutput = directory+'/temp/'
+def syncPrep(video1, video2):
+    tempOutput = os.path.join(os.path.dirname(video1), "temp")
     
     if not os.path.exists(tempOutput):
         os.makedirs(tempOutput)
     
-    print ffmpeg_prep(kinectInput, tempOutput + 'kinectMono.wav', 'prep')
-    if smiInput != None:
-        print ffmpeg_prep(smiInput, tempOutput + 'smiMono.wav', 'prep')
-    if moraeInput != None:
-        print ffmpeg_prep(moraeInput, tempOutput + 'moraeMono.wav', 'prep')
+    print ffmpeg_prep(video1, os.path.join(tempOutput,'v1.wav'), 'prep')
+    print ffmpeg_prep(video2, os.path.join(tempOutput,'v2.wav'), 'prep')
     
-    if smiInput != None:
-        print ffmpeg_merge(tempOutput + 'kinectMono.wav', tempOutput + 'smiMono.wav', tempOutput + 'mergeKS.wav', 'merge')
-    if moraeInput != None:
-        print ffmpeg_merge(tempOutput + 'kinectMono.wav', tempOutput + 'moraeMono.wav', tempOutput + 'mergeKM.wav', 'merge')
-     
-    if smiInput != None:
-        print vamp_sync(tempOutput + 'mergeKS.wav', tempOutput + 'KS.txt', 'sync')
-    if moraeInput != None:
-        print vamp_sync(tempOutput + 'mergeKM.wav', tempOutput + 'KM.txt', 'sync')
-    
+    print ffmpeg_merge(os.path.join(tempOutput,'v1.wav'), os.path.join(tempOutput,'v2.wav'), os.path.join(tempOutput,'merge.wav'), 'merge')
+
+    print vamp_sync(os.path.join(tempOutput,'merge.wav'), os.path.join(tempOutput,'merge.txt'), 'sync')
+
     return tempOutput
 
 
@@ -128,58 +112,21 @@ def syncTxt2csv(tempdirectory):
             out_csv.close()
 
 
-def main():
-    directory = tkFileDialog.askdirectory()
-    root.destroy()
-    
-    for dirContents in os.listdir(directory):
-        #print dirContents
-        if dirContents == 'Kinect':
-            kinectDirectory = directory+'/Kinect'
-            for filename in os.listdir(kinectDirectory):
-                if fnmatch.fnmatch(filename, '*.wav') and not fnmatch.fnmatch(filename, '._*'):
-                    #print filename
-                    kinectAudioFile = os.path.join(kinectDirectory, filename)
-                    
-        if dirContents == 'Morae':
-            moraeDirectory = directory+'/Morae'
-            for filename in os.listdir(moraeDirectory):
-                if fnmatch.fnmatch(filename, '*.mp4') and not fnmatch.fnmatch(filename, '._*'):
-                    #print filename
-                    moraeVideoFile = os.path.join(moraeDirectory, filename)
-            if os.listdir(moraeDirectory) == []:
-                moraeVideoFile = None
-                        
-        if dirContents == 'SMI':
-            smiDirectory = directory+'/SMI'
-            for filename in os.listdir(smiDirectory):
-                if fnmatch.fnmatch(filename, '*.mp4') and not fnmatch.fnmatch(filename, '._*'):
-                    #print filename
-                    smiVideoFile = os.path.join(smiDirectory, filename)
-            if os.listdir(smiDirectory) == []:
-                smiVideoFile = None
-    
-    return directory, kinectAudioFile, smiVideoFile, moraeVideoFile
-
-
-def syncMatch(directory, kinectAudio, smiVideo, moraeVideo):
-    tempSyncDir = syncPrep(directory, kinectAudio, smiVideo, moraeVideo)
+def syncMatch(video1, video2):
+    tempSyncDir = syncPrep(video1, video2)
     syncTxt2csv(tempSyncDir)
     syncHeader = "Data Stream, Sync Point (in seconds)\n"
     writeSyncHeader = True
     syncOutputData = []
-    syncOutputData.append("Kinect,0\n")
     
-    syncsavename = directory+"/syncData.csv"
+    syncsavename = os.path.join(os.path.dirname(video1), "syncData.csv")
     sync_file = r""+syncsavename+""
     with open(sync_file, 'wb') as syncOutput:
         syncWriter = csv.writer(syncOutput)
         for csvfile in os.listdir(tempSyncDir):
             #print csvfile
-            if fnmatch.fnmatch(csvfile, 'KM.csv'):
-                syncOutputData.append(findMode(tempSyncDir+'/'+csvfile, 'KM'))
-            if fnmatch.fnmatch(csvfile, 'KS.csv'):
-                syncOutputData.append(findMode(tempSyncDir+'/'+csvfile, 'KS'))
+            if fnmatch.fnmatch(csvfile, 'merge.csv'):
+                syncOutputData.append(findMode(os.path.join(tempSyncDir,csvfile)))
         
         if writeSyncHeader:
             syncWriter.writerow(syncHeader.split(','))
@@ -197,12 +144,8 @@ def syncMatch(directory, kinectAudio, smiVideo, moraeVideo):
     print "Synchronization Complete"
 
 
-
-histoFlag = True
-
-
-directory, kinectAudioFile, smiVideoFile, moraeVideoFile = main()
-
-syncMatch(directory, kinectAudioFile, smiVideoFile, moraeVideoFile)
-if histoFlag:
-    P.show()
+if __name__ == "__main__":
+    video1 = tkFileDialog.askopenfilename()
+    video2 = tkFileDialog.askopenfilename()	
+    root.destroy()
+    syncMatch(video1, video2)
